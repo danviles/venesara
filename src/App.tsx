d38920a00821e0ac2c1c708527970c7b5e1c5da0
Elvis TEC <danvilapps@gmail.com>
2026-01-17 21:17:25 +0100

    poema init

--- src/App.tsx
+++ src/App.tsx
@@ -11,56 +11,58 @@ type Screen =
   | { type: "question"; index: number }
 
 function App() {
-  const [screen, setScreen] = useState<Screen>({ type: "presentation" })
+  // const [screen, setScreen] = useState<Screen>({ type: "presentation" })
 
-  if (screen.type === "presentation") {
-    return (
-      <Presentation
-        onStart={() =>
-          setScreen({
-            type: "transition",
-            title: "Bienvenido",
-            text: "Prepárate para el juego",
-          })
-        }
-      />
-    )
-  }
+  // if (screen.type === "presentation") {
+  //   return (
+  //     <Presentation
+  //       onStart={() =>
+  //         setScreen({
+  //           type: "transition",
+  //           title: "Bienvenido",
+  //           text: "Prepárate para el juego",
+  //         })
+  //       }
+  //     />
+  //   )
+  // }
 
-  if (screen.type === "transition") {
-    return (
-      <Transition
-        title={screen.title}
-        text={screen.text}
-        onContinue={() =>
-          setScreen({ type: "question", index: 0 })
-        }
-      />
-    )
-  }
+  // if (screen.type === "transition") {
+  //   return (
+  //     <Transition
+  //       title={screen.title}
+  //       text={screen.text}
+  //       onContinue={() =>
+  //         setScreen({ type: "question", index: 0 })
+  //       }
+  //     />
+  //   )
+  // }
 
-  if (screen.type === "question") {
-    return (
-      <Question
-        data={questions[screen.index]}
-        onAnswer={() => {
-          const next = screen.index + 1
+  // if (screen.type === "question") {
+  //   return (
+  //     <Question
+  //       data={questions[screen.index]}
+  //       onAnswer={() => {
+  //         const next = screen.index + 1
 
-          if (next < questions.length) {
-            setScreen({ type: "question", index: next })
-          } else {
-            setScreen({
-              type: "transition",
-              title: "Fin",
-              text: "Gracias por jugar",
-            })
-          }
-        }}
-      />
-    )
-  }
+  //         if (next < questions.length) {
+  //           setScreen({ type: "question", index: next })
+  //         } else {
+  //           setScreen({
+  //             type: "transition",
+  //             title: "Fin",
+  //             text: "Gracias por jugar",
+  //           })
+  //         }
+  //       }}
+  //     />
+  //   )
+  // }
 
-  return null
+  // return null
+
+  return <div className="poema">Las mantarrallas no existen, las orcas son delfines, me encantas Sara porque tu sonrisa en mi corazón existe ❤️</div>
 }
 
-export default App
+    export default App
